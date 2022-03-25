75eda4be0b10231ebb2ffb2eb1ebe504d88a9fe5
Syarif <58346354+syarif-nj@users.noreply.github.com>
2022-03-25 21:51:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.Drivername = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectTaxi
+{
+    class Taxi
+    {
+        public string Drivername { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Nama Driver : {0}", Drivername);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number Of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} Sedanga Menjemput Penumpang", Drivername);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", Drivername);
+        }
+    }
+}
